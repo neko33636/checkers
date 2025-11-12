280d60ea5f0d6811f948f7f9540a28c4e04866ce
pocht <maksborodi@gmail.com>
2025-11-12 21:50:38 +0300

    добавил логику в класс move и piece

--- logic/src/main/java/org/examle/checkers/logic/Move.java
+++ logic/src/main/java/org/examle/checkers/logic/Move.java
@@ -1,4 +1,18 @@
-package org.examle.checkers.logic;
+package org.example.checkers.logic;
 
 public class Move {
+    private int fromRow, fromCol;
+    private int toRow, toCol;
+
+    public Move(int fromRow, int fromCol, int toRow, int toCol) {
+        this.fromRow = fromRow;
+        this.fromCol = fromCol;
+        this.toRow = toRow;
+        this.toCol = toCol;
+    }
+
+    public int getFromRow() { return fromRow; }
+    public int getFromCol() { return fromCol; }
+    public int getToRow() { return toRow; }
+    public int getToCol() { return toCol; }
 }
--- logic/src/main/java/org/examle/checkers/logic/Piece.java
+++ logic/src/main/java/org/examle/checkers/logic/Piece.java
@@ -1,4 +1,32 @@
-package org.examle.checkers.logic;
+package org.example.checkers.logic;
 
 public class Piece {
+    private String type;
+    private String color;
+
+    public Piece(String color) {
+        this.color = color.toUpperCase();
+        this.type = "MAN";
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void promoteToKing() {
+        this.type = "KING";
+    }
+
+    public boolean isKing() {
+        return type.equals("KING");
+    }
+
+    @Override
+    public String toString() {
+        return color + " " + type;
+    }
 }
